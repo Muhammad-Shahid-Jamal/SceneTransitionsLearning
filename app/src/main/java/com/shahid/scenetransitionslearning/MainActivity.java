756758b3797faa14ed20cab6acace62dcaac6dd8
shahid <shahid@braincrop.net>
2019-09-06 15:12:59 +0500

    bounce based animation complete learning

--- app/src/main/java/com/shahid/scenetransitionslearning/MainActivity.java
+++ app/src/main/java/com/shahid/scenetransitionslearning/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.transition.ChangeBounds;
 import android.transition.Scene;
 import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     ViewGroup rootContainer;
     Scene scene1,scene2;
-    Transition bound = new ChangeBounds();
+    Transition bound ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +23,15 @@ public class MainActivity extends AppCompatActivity {
         scene1 = Scene.getSceneForLayout(rootContainer,R.layout.scene1_layout,this);
         scene2 = Scene.getSceneForLayout(rootContainer,R.layout.scene2_layout,this);
         scene1.enter();
-
+//        bound= new ChangeBounds();
+        bound = TransitionInflater.from(this).inflateTransition(R.transition.transition);
     }
     public void goToScene1(View view){
-        bound.setDuration(500);
+//        bound.setDuration(500);
         TransitionManager.go(scene2,bound);
     }
     public void goToScene2(View view){
-        bound.setDuration(500);
+//        bound.setDuration(500);
         TransitionManager.go(scene1,bound);
     }
 }
