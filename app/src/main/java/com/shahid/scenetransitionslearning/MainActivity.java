1c13a92bf2c8f04386e353c21fe9800664dbe23b
shahid <shahid@braincrop.net>
2019-09-06 11:20:14 +0500

    scene 1 loaded success

--- app/src/main/java/com/shahid/scenetransitionslearning/MainActivity.java
+++ app/src/main/java/com/shahid/scenetransitionslearning/MainActivity.java
@@ -2,12 +2,18 @@ package com.shahid.scenetransitionslearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewGroup rootContainer;
+    Scene scene1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootContainer = (ViewGroup) findViewById(R.id.root_container);
+        scene1 = Scene.getSceneForLayout(rootContainer,R.layout.scene1_layout,this);
+        scene1.enter();
     }
 }
